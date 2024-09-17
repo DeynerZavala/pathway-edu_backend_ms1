c7dc5e87e0d9d56f14ed1d580baae0584e4688a1
DeynerZavala <dyr.zva.hyi@gmail.com>
2024-09-17 01:03:40 -0500

    Login

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS sin restricciones (NO RECOMENDADO PARA PRODUCCIÓN)
+  app.enableCors({
+    origin: '*', // Permite cualquier origen
+    methods: 'GET,POST,PUT,DELETE', // Permite estos métodos
+    allowedHeaders: '*', // Permite cualquier cabecera
+    credentials: false, // No requiere credenciales
+  });
+
   await app.listen(3000);
 }
 bootstrap();
